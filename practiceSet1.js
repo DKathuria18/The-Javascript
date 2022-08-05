e97ff08d725f53afdbc7db3c9726f3a1fe147aa4
Dhiraj Kathuria <78962786+DKathuria18@users.noreply.github.com>
2022-08-05 22:20:41 +0530

    Create practiceSet1.js

---
+++ practiceSet1.js
@@ -0,0 +1,31 @@
+// const { DEFAULT_ECDH_CURVE } = require("tls");
+
+let a = "dhruv"
+let b = 6;
+
+console.log(a + b)
+
+console.log(typeof (a + b) )
+
+const c = {
+    name: "dhruv",
+    section: 1,
+    isPrincipal: false
+}
+
+//  c = "dhruv"
+
+c['friend'] = "deepanshi"
+console.log(c)
+
+
+// dictionary
+const dict = {
+appreciate : " recognize the full woth of ",
+yakka: " work, espcially hard work"
+     
+}
+
+console.log(dict)
+
+console.log(dict.yakka)
