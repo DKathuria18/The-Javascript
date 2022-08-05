f3baa768079b00e5e3ceaf0155e2a278a632a1a8
Dhiraj Kathuria <78962786+DKathuria18@users.noreply.github.com>
2022-08-05 22:19:37 +0530

    Create var,let,const.js

---
+++ var,let,const.js
@@ -0,0 +1,33 @@
+console.log("javascript part 2");
+const author = 'Dhruv';
+console.log(author)
+// let author = "dhiraj"; 
+// it will show error because auhtoe has been declared and the value of const will never be change until you chanve the code
+
+
+//var is used globally in the code eg var b 
+// var a = 45;
+// var b = "Dhruv";
+// var c = null;
+// var d = undefined;
+
+// {
+//     var b = 'this';
+//     console.log(b)
+// }
+
+// console.log(b)
+
+
+// let is used for block elements for eg let b 
+// var a = 45;
+// let b = "Dhruv";
+// var c = null;
+// var d = undefined;
+
+// {
+//     let b = 'this';
+//     console.log(b)
+// }
+
+// console.log(b)
